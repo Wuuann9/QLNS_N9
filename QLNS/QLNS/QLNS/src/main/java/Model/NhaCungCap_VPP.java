a70fcbb3112b3dd892f00c030577fddd326ceb64
Wuaann9 <kviet6104@gmail.com>
2025-04-05 14:49:38 +0700

    Chỉnh sửa tìm kiếm VPP theo tên

--- QLNS/QLNS/QLNS/src/main/java/Model/NhaCungCap_VPP.java
+++ QLNS/QLNS/QLNS/src/main/java/Model/NhaCungCap_VPP.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Model;
 
-/**
- *
- * @author nguye
- */
+
 public class NhaCungCap_VPP {
     private String maNCCVPP ;
 	private String tenNCCVPP ;
