2dee42e21bab1931b107f79def1fe3efdd372579
wyn279 <nttquyen279@gmail.com>
2025-04-19 13:44:15 +0700

    Chinh sua

--- QLNS/QLNS/QLNS/src/main/java/Connect/NhanVien_Connect.java
+++ QLNS/QLNS/QLNS/src/main/java/Connect/NhanVien_Connect.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 import javax.swing.JOptionPane;
+// nv
 
 public class NhanVien_Connect extends Connect_sqlServer{
     private DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
