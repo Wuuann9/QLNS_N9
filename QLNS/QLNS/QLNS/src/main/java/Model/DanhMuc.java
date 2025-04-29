bd58eb2851f6411bc23930a89b43bc47254ef952
Wuaann9 <kviet6104@gmail.com>
2025-04-29 17:31:25 +0700

    delete maVach them danh muc

---
+++ QLNS/QLNS/QLNS/src/main/java/Model/DanhMuc.java
@@ -0,0 +1,67 @@
+
+package Model;
+
+
+public class DanhMuc {
+    private String maSach;
+    private String tenSach;
+    private String maNXB;
+    private String tenTacGia;
+    private String theLoai;
+    private int soLuong;
+    
+    public DanhMuc(){
+        super();
+    }
+
+    public String getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+
+    public String getTenSach() {
+        return tenSach;
+    }
+
+    public void setTenSach(String tenSach) {
+        this.tenSach = tenSach;
+    }
+
+    public String getMaNXB() {
+        return maNXB;
+    }
+
+    public void setMaNXB(String maNXB) {
+        this.maNXB = maNXB;
+    }
+
+    public String getTacGia() {
+        return tenTacGia;
+    }
+
+    public void setTacGia(String tenTacGia) {
+        this.tenTacGia = tenTacGia;
+    }
+
+    public String getTheLoai() {
+        return theLoai;
+    }
+
+    public void setTheLoai(String theLoai) {
+        this.theLoai = theLoai;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+   
+    
+}
