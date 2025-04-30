ef6ca27c64e3bd700ed8f55bb57937b7b3861584
Datnt <dat8b17112003@gmail.com>
2025-04-30 23:25:59 +0700

    update file CTHD_Connect.java

--- QLNS/QLNS/QLNS/src/main/java/Connect/CTHD_Connect.java
+++ QLNS/QLNS/QLNS/src/main/java/Connect/CTHD_Connect.java
@@ -15,7 +15,8 @@ import javax.swing.table.DefaultTableModel;
 
 // thao tác với bảng CTHD trong CSDL
 public class CTHD_Connect extends Connect_sqlServer{
-    // thêm một dòng chi tiết hóa đơn vào bảng CTHD.
+    
+    // thêm một sách/vpp chi tiết hóa đơn vào bảng CTHD.
     public int ThemCT(CTHD cthd) {
         try {	
             String sql = "insert into CTHD values(?,?,?,?,?) " ; // Tạo câu lệnh SQL với 5 dấu ? (tương ứng 5 cột cần điền trong bảng CTHD)
@@ -36,22 +37,30 @@ public class CTHD_Connect extends Connect_sqlServer{
         }
         return -1 ;
     }
-    
+   
+    // Lấy ra CTHD bằng MaHD
     public DefaultTableModel layCTHDBangMaHD(String mahd){
-        DefaultTableModel dtm_cthd = new DefaultTableModel();
+        // DefaultTableModel - một lớp trong Java Swing, được sử dụng để lưu trữ dữ liệu dạng bảng (các hàng và cột) để hiển thị trên JTable.
+        //1.  Tạo bảng CTHD gồm 4 cột
+        DefaultTableModel dtm_cthd = new DefaultTableModel(); // tạo 1 bảng trống
         dtm_cthd.addColumn("Tên sản phẩm");
         dtm_cthd.addColumn("Đơn giá");
         dtm_cthd.addColumn("Số lượng");
         dtm_cthd.addColumn("Thành tiền");
-        dtm_cthd.setRowCount(0);
+        dtm_cthd.setRowCount(0); // Đặt số hàng của bảng về 0 (xóa toàn bộ dữ liệu hiện tại nếu có) => bảng bắt đầu trống trước khi thêm dữ liệu mới.
+        // 2. Lấy data từ CSDL => lưu kết quả và ResultSet
         try {	
             String sql = "SELECT S.TenSach, V.TenVPP, C.SoLuong, C.DonGia, C.ThanhTien  FROM HOADON AS H LEFT JOIN CTHD AS C ON H.MaHD= C.MaHD LEFT JOIN SACH AS S ON C.MaSP=S.MaSach LEFT JOIN VPP AS V ON C.MaSP=V.MaVPP WHERE H.NhapSach=0 AND H.ThanhCong=1 AND H.MaHD = ?";
             PreparedStatement pre = conn.prepareStatement(sql);
             pre.setString(1, mahd);
             ResultSet result  = pre.executeQuery();
+            // 3. lấy kết quả từ ResultSet vào bảng CTHD
             while(result.next())
+            // 1 là TenSach,2 là TenVPP,3 là Soluong, 4 là DonGia, 5 là ThanhTien
             {
                 Vector<Object> vec = new Vector<Object>();
+                // Vì một sản phẩm chỉ có thể là sách hoặc văn phòng phẩm (không thể là cả hai),
+                //=>  một trong hai cột TenSach hoặc TenVPP sẽ là null.
                 if(result.getString(1)!=null)
                     vec.add(result.getString(1));
                 if(result.getString(2)!=null)
